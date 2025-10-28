ec06d9def2582b9e13ad5931507d5f8ca70bb3ae
serhii.zavada <serhii.zavada@tentens.tech>
2025-10-28 13:50:20 +0200

    wip

--- process_test.go
+++ process_test.go
@@ -58,3 +58,26 @@ func TestGetProcessInfo(t *testing.T) {
 	t.Logf("  IsWow64: %v", info.Win.IsWow64 != 0)
 	t.Logf("  PEB: 0x%X", info.Win.PEB)
 }
+
+func TestGetModuleList(t *testing.T) {
+	vmm := setupVmm(t)
+	defer vmm.Close()
+
+	pid, err := vmm.GetPidByName("explorer.exe")
+	if err != nil {
+		t.Fatalf("GetPidByName(\"explorer.exe\") failed: %v", err)
+	}
+
+	moduleList, err := vmm.GetModuleList(pid)
+	if err != nil {
+		t.Fatalf("GetModuleList failed: %v", err)
+	}
+
+	if moduleList.Version != MapModuleVersion {
+		t.Errorf("ModuleList version mismatch: expected %d, got %d", MapModuleVersion, moduleList.Version)
+	}
+
+	if len(moduleList.Modules) != int(moduleList.Count) {
+		t.Errorf("Module count mismatch: expected %d, got %d", moduleList.Count, len(moduleList.Modules))
+	}
+}
