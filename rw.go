346388fd244e41185ca28c3ba10d172b0799e212
serhii.zavada <serhii.zavada@tentens.tech>
2026-03-14 01:51:55 +0200

    feat: implement MemWrite (VMMDLL_MemWrite)

--- rw.go
+++ rw.go
@@ -5,6 +5,16 @@ import (
 	"unsafe"
 )
 
+func (vmm *Vmm) MemWrite(pid uint32, addr uint64, data []byte) error {
+	if len(data) == 0 {
+		return nil
+	}
+	if !vmmMemWrite(vmm.vmmHandle, pid, addr, unsafe.Pointer(&data[0]), uint32(len(data))) {
+		return fmt.Errorf("failed to write memory to PID %d at address 0x%X", pid, addr)
+	}
+	return nil
+}
+
 func (vmm *Vmm) MemRead(pid uint32, addr uint64, size uint32) ([]byte, error) {
 	buffer := make([]byte, size)
 	success := vmmMemRead(vmm.vmmHandle, pid, addr, unsafe.Pointer(&buffer[0]), size)
