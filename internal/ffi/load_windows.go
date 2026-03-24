e5901642d5a1afe0d2b9ba7dcea1c1e75258e6ec
serhii.zavada <serhii.zavada@tentens.tech>
2026-03-24 20:06:37 +0200

    wip

--- internal/ffi/load_windows.go
+++ internal/ffi/load_windows.go
@@ -6,6 +6,9 @@ import "golang.org/x/sys/windows"
 
 // OpenLibrary loads the shared library at the given path.
 func OpenLibrary(name string) (uintptr, error) {
-	handle, err := windows.NewLazyDLL(name).Load()
-	return uintptr(handle), err
+	dll := windows.NewLazyDLL(name)
+	if err := dll.Load(); err != nil {
+		return 0, err
+	}
+	return uintptr(dll.Handle), nil
 }
